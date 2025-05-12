939ab6b7d2756b4dd5c0979ea3c9c8b7ef9c89c2
ToniR90 <romy90.t@gmail.com>
2025-05-12 10:18:52 +0200

    test added

---
+++ src/test/java/org/example/PlanetFinderTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlanetFinderTest {
+
+    @org.junit.jupiter.api.Test
+    void findPlanets() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findPlanetsWithAtmosphere() {
+    }
+}
